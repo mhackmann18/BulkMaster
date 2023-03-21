bb179f4943db4faf2221ba1e4ccd82a24f1acef2
Matt Hackmann <mhackmann13@icloud.com>
2023-03-20 19:01:00 -0500

    Created UI mockup

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+
+
+function Header() {
+  return (
+    <header id="main-header">
+      <h1>BULKMASTER</h1>
+      <ul>
+        <li>About</li>
+        <li>Sign Up</li>
+        <li>Log In</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
