f73f7e6a04c88963bc98f3b4a0e14d9fcb63ebea
Matt Hackmann <mhackmann13@icloud.com>
2023-03-21 19:54:27 -0500

    Created popup window component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import './App.css'
 import Banner from './Banner';
 import Button from './Button';
 import URLInput from './URLInput';
+import PopupWindow from './PopupWindow';
+import { useState } from 'react';
 
 function App() {
+  const [openPopup, setOpenPopup] = useState(false);
+  function fdsdfasd() {
+    console.log('dsa');
+    setOpenPopup(true);
+  }
+  console.log(openPopup);
   return (
     <div className="App">
       <div className="container">
-        <Banner />     
+        <Banner />
+        <PopupWindow isOpening={openPopup} setIsOpening={setOpenPopup}/>
         <main>
           <div>
             <p>Meal prepping on a bulk<br></br> just got easier.</p>
             <URLInput />
-            <Button text='Get Recipe' onClick={()=>{console.log('Get recipe')}}/>
+            <Button text='Get Recipe' onClick={fdsdfasd}/>
           </div>
         </main>
       </div>
---
+++ src/PopupWindow.js
@@ -0,0 +1,41 @@
+import './PopupWindow.css'
+import { useEffect } from 'react';
+
+// Popup html and js from https://codepen.io/Dr_rakcha/pen/eyyqbg
+function PopupWindow({ isOpening, setIsOpening}) {
+  
+  useEffect(() => {
+    if(isOpening){
+      setIsOpening(false);
+      openPopup();
+    }
+    console.log('Popup mounted');
+  }, []);
+
+  return (
+    <div id="popup-container" onClick={closePopup}>
+      <div className="popup" onClick={e => e.stopPropagation()}>
+        <button>Send</button>
+      </div>
+    </div>
+  );
+}
+
+function openPopup() {
+  let popupContainer = document.getElementById('popup-container');
+
+  popupContainer.style.display = 'block';
+
+  // fade in
+  setTimeout(() => popupContainer.style.opacity = 1, 100);
+}
+
+function closePopup(e){
+  // fade out 
+   e.target.style.opacity = 0;
+   setTimeout(function () {
+     e.target.style.display = 'none'
+   }, 500);
+}
+
+export default PopupWindow;
