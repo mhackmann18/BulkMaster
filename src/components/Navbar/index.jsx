f3352129ffc8f750e2be237259c2523f712c1542
Matt Hackmann <mhackmann13@icloud.com>
2023-04-16 11:19:10 -0500

    Style navbar links

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,19 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./index.css";
 
 export default function Navbar() {
+  const [activeLink, setActiveLink] = useState("");
+
   return (
-    <header id="main-nav">
-      <h1>PREPMASTER</h1>
+    <header id="navbar">
+      <Link id="navbar-logo" to="" onClick={() => setActiveLink("")}>
+        PREPMASTER
+      </Link>
       <ul>
         <li>
-          <Link to="about">About</Link>
+          <Link
+            to="about"
+            className={activeLink === "about" ? "active" : ""}
+            onClick={() => setActiveLink("about")}
+          >
+            About
+          </Link>
         </li>
         <li>
-          <Link to="signup">Sign Up</Link>
+          <Link
+            to="signup"
+            className={activeLink === "signup" ? "active" : ""}
+            onClick={() => setActiveLink("signup")}
+          >
+            Sign Up
+          </Link>
         </li>
         <li>
-          <Link to="login">Log In</Link>
+          <Link
+            to="login"
+            className={activeLink === "login" ? "active" : ""}
+            onClick={() => setActiveLink("login")}
+          >
+            Log In
+          </Link>
         </li>
       </ul>
     </header>
