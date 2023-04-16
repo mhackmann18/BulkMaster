a6025436091fec9801438a98cbc4914cef75329e
Matt Hackmann <mhackmann13@icloud.com>
2023-04-16 11:34:55 -0500

    Remove unnecessary state from Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,41 +1,31 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./index.css";
 
 export default function Navbar() {
-  const [activeLink, setActiveLink] = useState("");
+  const currentPath = useLocation().pathname;
 
   return (
     <header id="navbar">
-      <Link id="navbar-logo" to="" onClick={() => setActiveLink("")}>
+      <Link id="navbar-logo" to="">
         PREPMASTER
       </Link>
       <ul>
         <li>
-          <Link
-            to="about"
-            className={activeLink === "about" ? "active" : ""}
-            onClick={() => setActiveLink("about")}
-          >
+          <Link to="about" className={currentPath === "/about" ? "active" : ""}>
             About
           </Link>
         </li>
         <li>
           <Link
             to="signup"
-            className={activeLink === "signup" ? "active" : ""}
-            onClick={() => setActiveLink("signup")}
+            className={currentPath === "/signup" ? "active" : ""}
           >
-            Sign Up
+            Sign up
           </Link>
         </li>
         <li>
-          <Link
-            to="login"
-            className={activeLink === "login" ? "active" : ""}
-            onClick={() => setActiveLink("login")}
-          >
-            Log In
+          <Link to="login" className={currentPath === "/login" ? "active" : ""}>
+            Log in
           </Link>
         </li>
       </ul>
