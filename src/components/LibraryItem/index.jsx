ee5ffd17b9700931b935a7a8c5b2d7c1d170ca29
Matt Hackmann <mhackmann13@icloud.com>
2023-04-27 13:49:48 -0500

    Add ConfirmationModal to LibraryItem onDelete

---
+++ src/components/LibraryItem/index.jsx
@@ -0,0 +1,86 @@
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faClone,
+  faTrashCan,
+  faPenToSquare,
+} from "@fortawesome/free-solid-svg-icons";
+import "./index.css";
+
+export default function LibraryItem({
+  recipeId,
+  recipeTitle,
+  recipeServings,
+  caloriesPerRecipeServing,
+  setDeleteModal,
+}) {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="library-item"
+      onClick={() => navigate(`/dashboard/recipe-library/${recipeId}`)}
+    >
+      <div className="left">
+        <h2>{recipeTitle}</h2>
+        <div className="row">
+          <span>Servings: {recipeServings}</span>
+          {caloriesPerRecipeServing ? (
+            <span>Calories per Serving: {caloriesPerRecipeServing}</span>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+      <div className="right">
+        <FontAwesomeIcon
+          icon={faTrashCan}
+          className="option-btn btn"
+          title="Delete"
+          size="1x"
+          onClick={(e) => {
+            setDeleteModal({
+              open: true,
+              itemId: recipeId,
+              message: `Are you sure you want to delete the recipe '${recipeTitle}'?`,
+            });
+            e.stopPropagation();
+          }}
+        />
+        <FontAwesomeIcon
+          icon={faClone}
+          className="option-btn btn"
+          title="Duplicate"
+          size="1x"
+          onClick={(e) =>
+            console.log(`Duplicate recipe with id: ${recipeId}`) ||
+            e.stopPropagation()
+          }
+        />
+        <FontAwesomeIcon
+          icon={faPenToSquare}
+          className="option-btn btn"
+          title="Edit"
+          size="1x"
+          onClick={(e) =>
+            console.log(`Edit recipe with id: ${recipeId}`) ||
+            e.stopPropagation()
+          }
+        />
+      </div>
+    </div>
+  );
+}
+
+LibraryItem.propTypes = {
+  recipeId: PropTypes.number.isRequired,
+  recipeTitle: PropTypes.string.isRequired,
+  recipeServings: PropTypes.number.isRequired,
+  caloriesPerRecipeServing: PropTypes.number,
+  setDeleteModal: PropTypes.func.isRequired,
+};
+
+LibraryItem.defaultProps = {
+  caloriesPerRecipeServing: 0,
+};
--- src/routes/Library.jsx
+++ src/routes/Library.jsx
@@ -1,20 +1,45 @@
-import RecipeItem from "../components/RecipeItem";
+import { useState } from "react";
+import LibraryItem from "../components/LibraryItem";
+import ConfirmationModal from "../components/common/ConfirmationModal";
 import data from "../assets/data.json";
+import "./Library.css";
 
 export default function Library() {
+  const [deleteModal, setDeleteModal] = useState({
+    open: false,
+    itemId: null,
+    message: "",
+  });
   const recipes = data;
 
   return (
     <div id="library-page">
       {recipes.map((recipe) => (
-        <RecipeItem
+        <LibraryItem
           key={recipe.id}
           recipeTitle={recipe.title}
           recipeServings={recipe.servings}
-          caloriesPerRecipeServing={recipe.nutrients.calories.quantity}
+          caloriesPerRecipeServing={
+            recipe.nutrients &&
+            recipe.nutrients.calories &&
+            recipe.nutrients.calories.quantity
+          }
           recipeId={recipe.id}
+          setDeleteModal={setDeleteModal}
         />
       ))}
+
+      <ConfirmationModal
+        open={deleteModal.open}
+        headerText="Delete Recipe"
+        messageText={deleteModal.message}
+        cancelBtnText="Cancel"
+        confirmBtnText="Delete"
+        onCancel={() => setDeleteModal({ ...deleteModal, open: false })}
+        onConfirm={() =>
+          console.log(`Delete item with id: ${deleteModal.itemId}`)
+        }
+      />
     </div>
   );
 }
