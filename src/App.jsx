922ce42142f0033cd4a54dc885c37cdda7af896a
Matt Hackmann <mhackmann13@icloud.com>
2023-05-02 10:08:13 -0500

    Get things up to date

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
+import UserContextProvider from "./UserContextProvider";
+import ThemeContextProvider from "./ThemeContextProvider";
 import Splash from "./routes/Splash";
 import Dashboard from "./routes/Dashboard";
 import Import from "./routes/Import";
@@ -8,15 +10,13 @@ import Library from "./routes/Library";
 import Settings from "./routes/Settings";
 import Recipe from "./routes/Recipe";
 import ImportedRecipe from "./routes/ImportedRecipe";
-import "./App.css";
 import Home from "./routes/Home";
 import Features from "./routes/Features";
 import Signup from "./routes/Signup";
 import Login from "./routes/Login";
 import disableInputScroll from "./utils/disableInputScroll";
 import theme from "./theme";
-import UserContextProvider from "./UserContextProvider";
-import ThemeContextProvider from "./ThemeContextProvider";
+import "./App.css";
 
 export default function App() {
   // Stop number input value from changing on scroll for all number inputs
