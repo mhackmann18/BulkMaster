12bc1b1db9613798b3052450bc8ee15fd4372293
Matt Hackmann <mhackmann13@icloud.com>
2023-06-02 12:27:37 -0500

    Add markup to features page

--- src/routes/Features.jsx
+++ src/routes/Features.jsx
@@ -1,3 +1,20 @@
+import "./Features.css";
+
 export default function Features() {
-  return <div id="features-page">Features</div>;
+  return (
+    <div id="features-page">
+      <div id="fp-intro-container">
+        <h2>Make meal prepping your favorite recipes a breeze</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error cum
+          nisi tempore numquam vel, consequatur odit exercitationem fugiat
+          quaerat! Deleniti.
+        </p>
+        {/* Make this a link */}
+        <button className="btn-main" type="button">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
 }
