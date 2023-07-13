e46058ebcd901152f148d08742830b4cb0b9fb28
Matt Hackmann <mhackmann13@icloud.com>
2023-07-13 18:43:32 -0500

    add confirmation modal for deleting account in settings

--- src/routes/Settings.jsx
+++ src/routes/Settings.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import SettingsList from "../components/SettingsList";
 import Button from "../components/common/Button";
 import useUser from "../hooks/useUser";
+import StandardModal from "../components/common/StandardModal";
+import ConfirmationDisplay from "../components/common/ConfirmationDisplay";
 import "./Settings.css";
 
 export default function Settings() {
+  const [modalOpen, setModalOpen] = useState(false);
   const navigate = useNavigate();
   const { setUser } = useUser();
+
+  const handleAccountDeletion = () => {
+    console.log("Delete account");
+  };
 
   return (
     <div id="settings-page">
@@ -26,11 +34,21 @@ export default function Settings() {
           />
           <Button
             text="Delete Account"
-            handleClick={() => {}}
+            handleClick={() => setModalOpen(true)}
             variant="default wide"
           />
         </div>
       </div>
+      <StandardModal open={modalOpen} handleClose={() => setModalOpen(false)}>
+        <ConfirmationDisplay
+          headerText="Confirm Account Deletion"
+          messageText="Your account info and all recipes in your library will be permanently deleted."
+          cancelBtnText="Cancel"
+          confirmBtnText="Delete"
+          onCancel={() => setModalOpen(false)}
+          onConfirm={handleAccountDeletion}
+        />
+      </StandardModal>
     </div>
   );
 }
