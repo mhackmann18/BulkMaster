4bcc90eaf8d8c4207116b0f0c36849537cf9686a
Matt Hackmann <mhackmann13@icloud.com>
2023-07-19 17:25:43 -0500

    style updates for home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,19 +6,22 @@ export default function Home() {
   return (
     <div id="home-page">
       <div id="home-window-1">
-        <div className="left-content">
-          <img className="reaper-img" src={reaper} alt="mascot" />
+        <div className="h-box">
+          <div className="left-content">
+            <img className="reaper-img" src={reaper} alt="mascot" />
+          </div>
+          <div className="right-content">
+            <h2 className="title">
+              Harvest deathly good recipes from across the web
+            </h2>
+            <p>
+              No need to scroll through ads and walls of text to reach the
+              recipe. RecipeReaper will find it for you.
+            </p>
+            <RecipeScrapingForm variant="inline" />
+          </div>
         </div>
-        <div className="right-content">
-          <h2 className="title">
-            Harvest deathly good recipes from across the web
-          </h2>
-          <p>
-            No need to scroll through ads and walls of text to reach the recipe.
-            RecipeReaper will find it for you.
-          </p>
-          <RecipeScrapingForm variant="inline" />
-        </div>
+        <div>icon</div>
       </div>
     </div>
   );
