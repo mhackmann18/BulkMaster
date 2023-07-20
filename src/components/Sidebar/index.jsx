85d3c554efcca0b9ca107778ac6a7ba8dd1dcec9
Matt Hackmann <mhackmann13@icloud.com>
2023-07-20 09:57:09 -0500

    change app name to 'RecipeReaper'

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -34,7 +34,7 @@ export default function Sidebar({ username, collapsed, setCollapsed }) {
         <h1>
           {!collapsed && (
             <span id="sidebar-logo">
-              RECIPE<span>REPO</span>
+              RECIPE<span>REAPER</span>
             </span>
           )}{" "}
           <FontAwesomeIcon
