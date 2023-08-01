3e2115be4880fba99c2e0977c28901f07489d1c2
Matt Hackmann <mhackmann13@icloud.com>
2023-08-01 13:27:40 -0500

    remove config from gitignore

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+  API_ORIGIN: "https://api.recipereaper.com:8080",
+  DOMAIN: "recipereaper.com",
+
+  // ERROR CODES
+  INVALID_TOKEN_ERR: "invalid-token",
+  NO_TOKEN_ERR: "no-token",
+  NO_PERMISSION_ERR: "permission-denied",
+};
